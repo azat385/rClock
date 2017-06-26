6ff9d4eed1dd55bf0b05aef0be25e2a5505b8932
VDS <VDS>
2017-06-26 18:07:26 +0300

    first commit

--- mb.py
+++ mb.py
@@ -38,5 +38,10 @@ data = s.recv(BUFFER_SIZE)
 s.close()
 print "write: {}\nread:  {}".format(hexString(getPowerCRC),hexString(data))
 
-# data = unpack(">BBBHHBB", data)
-# print "{}ppm {}°C".format(data[3]/10, data[4]/10)
+data = unpack(">BBBHHBB", data)
+print "{}ppm {}°C".format(data[3]/10, data[4]/10)
+
+import memcache
+mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+mc.set("CO2", data[3]/10)
+mc.set("T", data[4]/10.0)
